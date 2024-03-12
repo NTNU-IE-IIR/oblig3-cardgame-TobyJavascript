1b3f56f70008763722906a98df2372e4e0d50e8f
Tobias <tobiaor@stud.ntnu.no>
2024-03-12 17:03:19 +0100

    Added class for evaluating card value

---
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/CardEvaluator.java
@@ -0,0 +1,19 @@
+package no.ntnu.idatx2003.oblig3.cardgame;
+
+public class CardEvaluator {
+
+    public boolean checkIfFlush () {
+        boolean flush = false;
+        return flush;
+    }
+
+    public boolean checkIfHeart () {
+        boolean heart = false;
+        return heart;
+    }
+
+    public boolean checkIfQueenSpade () {
+        boolean queenSpade = false;
+        return queenSpade;
+    }
+}
--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/GameGUI.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/GameGUI.java
@@ -1,19 +1,17 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GameGUI extends Application {
-    // Initializes playing card deck
+    // Initializes playing card deck and card evaluator
     PlayingCardDeck playingCardDeck = new PlayingCardDeck();
+    CardEvaluator cardEvaluator = new CardEvaluator();
 
     // Variable field
     private Label cardSlot1;
@@ -21,6 +19,9 @@ public class GameGUI extends Application {
     private Label cardSlot3;
     private Label cardSlot4;
     private Label cardSlot5;
+    private Label queenSpadeBox;
+    private Label heartBox;
+    private Label flushBox;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -28,19 +29,29 @@ public class GameGUI extends Application {
         BorderPane root = new BorderPane();
         VBox vButtonBox = new VBox();
         HBox hLabelBox = new HBox();
+        HBox hSpecialCardBox = new HBox();
 
         // Creates buttons
-        Button button1 = new Button("Print cards");
-        Button button2 = new Button("Shuffle");
+        Button button1 = new Button("Deal cards");
+        Button button2 = new Button("Show cards");
 
-        //Creates card slots
+        // Creates card slots
         cardSlot1 = new Label("");
         cardSlot2 = new Label("");
         cardSlot3 = new Label("");
         cardSlot4 = new Label("");
         cardSlot5 = new Label("");
 
+        // Create special card info boxes
+        queenSpadeBox = new Label("");
+        heartBox = new Label("");
+        flushBox = new Label("");
+
+        // Adds class to root
+        root.getStyleClass().add("body");
+
         // Adds events and classes to buttons
+        vButtonBox.getStyleClass().add("vButtonBox");
         button1.getStyleClass().add("custom-button"); // Adding a custom CSS class
         button2.getStyleClass().add("custom-button"); // Adding a custom CSS class
         vButtonBox.getStyleClass().add("vButtonBox"); // Adding a custom CSS class
@@ -53,6 +64,9 @@ public class GameGUI extends Application {
         cardSlot3.getStyleClass().add("label");
         cardSlot4.getStyleClass().add("label");
         cardSlot5.getStyleClass().add("label");
+        queenSpadeBox.getStyleClass().add("specialCardBoxes");
+        heartBox.getStyleClass().add("specialCardBoxes");
+        flushBox.getStyleClass().add("specialCardBoxes");
 
         // Style via java
         vButtonBox.setSpacing(10);
@@ -62,9 +76,10 @@ public class GameGUI extends Application {
         root.setRight(vButtonBox);
         hLabelBox.getChildren().addAll(cardSlot1,cardSlot2,cardSlot3,cardSlot4,cardSlot5);
         root.setCenter(hLabelBox);
+        hSpecialCardBox.getChildren().addAll(queenSpadeBox,heartBox,flushBox);
+        root.setBottom(hSpecialCardBox);
 
-
-        Scene scene = new Scene(root, 700, 500);
+        Scene scene = new Scene(root, 1000, 700);
         scene.getStylesheets().add("style.css");
 
         primaryStage.setTitle("Card game ultimate");
@@ -74,14 +89,36 @@ public class GameGUI extends Application {
 
     private void handleClick() {
         playingCardDeck.shuffle();
+        cardSlot1.setText("?");
+        cardSlot2.setText("?");
+        cardSlot3.setText("?");
+        cardSlot4.setText("?");
+        cardSlot5.setText("?");
+
+        queenSpadeBox.setText("Queen spade status: false");
+        heartBox.setText("Cards that are heart: ");
+        flushBox.setText("Flush status: ");
+    }
+    private void handleClick2() {
         cardSlot1.setText(playingCardDeck.getCardBasedOnIndex(0).getAsString());
         cardSlot2.setText(playingCardDeck.getCardBasedOnIndex(1).getAsString());
         cardSlot3.setText(playingCardDeck.getCardBasedOnIndex(2).getAsString());
         cardSlot4.setText(playingCardDeck.getCardBasedOnIndex(3).getAsString());
         cardSlot5.setText(playingCardDeck.getCardBasedOnIndex(4).getAsString());
+
+        checkAfterSpecialCards();
     }
-    private void handleClick2() {
-        playingCardDeck.shuffle();
+
+    private void checkAfterSpecialCards () {
+        if (cardEvaluator.checkIfFlush()) {
+
+        }
+        if (cardEvaluator.checkIfQueenSpade()) {
+
+        }
+        if (cardEvaluator.checkIfHeart()) {
+
+        }
     }
 
     @Override
